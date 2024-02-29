25f4220e113b251e32787177a60a428e294d549e
LiantsoaRasolofoson <liantsoarasolofoson@gmail.com>
2024-03-01 00:33:03 +0300

    Modification logo

--- magneva-frontoffice/src/app/template/footer/footer.component.ts
+++ magneva-frontoffice/src/app/template/footer/footer.component.ts
@@ -12,7 +12,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-  description = "MAGNEVA est une salon de beauté";
+  description = "MAGNEVA est un salon de beauté situé à Madagascar, spécialisé dans une gamme complète de services de soins esthétiques et de bien-être. Notre équipe dévouée est prête à vous offrir une expérience luxueuse et revitalisante, où vous pourrez vous détendre et vous faire chouchouter dans un cadre élégant et apaisant.";
   mail = "magneva@gmail.com";
   phone = "0348615850";
 }
